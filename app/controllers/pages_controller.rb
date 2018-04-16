81b192ad8eaf746e20a5fbe1b71f5b5a6ad39868
Joe Calhoun <joeycalhoun13@gmail.com>
2018-04-16 03:52:43 +0000

    Add search functionality in navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,11 @@ class PagesController < ApplicationController
    def about
        
    end
+   
+   def search
+      @search_term = params[:search]
+      @articles =Article.where("title LIKE ? OR description LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
+      @users = User.where("username LIKE ? OR email LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
+      @categories = Category.where("name LIKE ?", "%#{params[:search]}%")
+   end
 end
